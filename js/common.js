01e24b76f05070f636c755de259a11b5441efbf0
yewul01 <68521830+yewul01@users.noreply.github.com>
2020-09-10 17:41:30 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -9,13 +9,6 @@
         $('#joBox').load(url)
     })
 
-    $('h1NavTop > h1 > a').on('click', function(e) {
-        e.preventDefault()
-        var url = $(this).attr('href')
-        $('#joContainer').remove()
-        $('#joBox').load(url)
-    })
-
 
 
 
